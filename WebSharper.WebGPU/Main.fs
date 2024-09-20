b131eefa749612bfc60b7f632f17a0b34689dd9d
Souvanxay LUANGSOUVANNAVONG <souvanxay38@gmail.com>
2024-09-20 16:53:26 +0200

    Initial commit

---
+++ WebSharper.WebGPU/Main.fs
@@ -0,0 +1,24 @@
+﻿namespace WebSharper.WebGPU
+
+open WebSharper
+open WebSharper.JavaScript
+open WebSharper.InterfaceGenerator
+
+module Definition =
+
+
+    let Assembly =
+        Assembly [
+            Namespace "WebSharper.WebGPU" [
+                 
+            ]
+        ]
+
+[<Sealed>]
+type Extension() =
+    interface IExtension with
+        member ext.Assembly =
+            Definition.Assembly
+
+[<assembly: Extension(typeof<Extension>)>]
+do ()
